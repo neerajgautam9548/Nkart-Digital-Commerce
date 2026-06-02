d92e94963fb538be946c0c2b0821e350c1c25663
neeraj gautam <neerajgautam@gmail.com>
2026-06-02 11:27:49 +0530

    new package

--- backend/routes/reqOtp.js
+++ backend/routes/reqOtp.js
@@ -11,7 +11,7 @@ const generateOTP = () => {
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,      // SMTP Login
@@ -22,6 +22,26 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASSWORD:", process.env.EMAIL_PASSWORD);
 console.log("SENDER_EMAIL:", process.env.SENDER_EMAIL);
 
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+
+const client = SibApiV3Sdk.ApiClient.instance;
+
+client.authentications['api-key'].apiKey =
+process.env.BREVO_API_KEY;
+
+// const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+// await apiInstance.sendTransacEmail({
+//   sender: {
+//     email: process.env.SENDER_EMAIL,
+//     name: "Neeraj Bazar Store"
+//   },
+//   to: [{
+//     email: email
+//   }],
+//   subject: "OTP Verification",
+//   htmlContent: `<h2>Your OTP is ${otp}</h2>`
+// });
 
 const sendOTP = async (email, otp) => {
   try {
