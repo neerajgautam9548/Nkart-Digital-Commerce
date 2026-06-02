f94292684d500f8445fb896afe523b7f215575ef
neeraj gautam <neerajgautam@gmail.com>
2026-06-02 09:29:08 +0530

    new reqopt

--- backend/routes/reqOtp.js
+++ backend/routes/reqOtp.js
@@ -11,13 +11,12 @@ const generateOTP = () => {
 
 // transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  },
-  tls: {
-    rejectUnauthorized: false,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
